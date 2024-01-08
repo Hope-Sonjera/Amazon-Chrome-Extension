1577eb2692883f3f757a7b9b6c5e81525711d878
Hope Sonjera <hpsonjera@gmail.com>
2024-01-08 10:51:21 +0100

    Another Commit

--- Frontend/amazon/public/background.js
+++ Frontend/amazon/public/background.js
@@ -1,8 +1,7 @@
 // frontend/my-extension/public/background.js
 chrome.runtime.onInstalled.addListener(function(details) {
-    if (details.reason === 'install' || details.reason === 'update') {
-      // Open a new tab with the installation URL
-      chrome.tabs.create({ url: 'https://your-extension-installation-url' });
-    }
-  });
-  
+  if (details.reason === 'install' || details.reason === 'update') {
+    // Open the options page for your extension
+    chrome.runtime.openOptionsPage();
+  }
+});
